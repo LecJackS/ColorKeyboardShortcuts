04c6d3391cba65ca413ab243e689139f23974fd6
jack <jacksparrow.88@gmail.com>
2021-04-25 11:25:29 -0300

    Configured for 10 colors for a dark paper palette

--- ShortcutsPlugin/main.lua
+++ ShortcutsPlugin/main.lua
@@ -13,12 +13,80 @@ function initUi()
   app.registerUi({["menu"] = "Color #2", ["callback"] = "color2", ["accelerator"] = "<Alt>2"});
   app.registerUi({["menu"] = "Color #3", ["callback"] = "color3", ["accelerator"] = "<Alt>3"});
   app.registerUi({["menu"] = "Color #4", ["callback"] = "color4", ["accelerator"] = "<Alt>4"});
+  app.registerUi({["menu"] = "Color #5", ["callback"] = "color5", ["accelerator"] = "<Alt>5"});
+  app.registerUi({["menu"] = "Color #6", ["callback"] = "color6", ["accelerator"] = "<Alt>6"});
+  app.registerUi({["menu"] = "Color #7", ["callback"] = "color7", ["accelerator"] = "<Alt>7"});
+  app.registerUi({["menu"] = "Color #8", ["callback"] = "color8", ["accelerator"] = "<Alt>8"});
+  app.registerUi({["menu"] = "Color #9", ["callback"] = "color9", ["accelerator"] = "<Alt>9"});
+  app.registerUi({["menu"] = "Color #0", ["callback"] = "color0", ["accelerator"] = "<Alt>0"});
+
+  app.registerUi({["menu"] = "Set up keyboard shortcuts", ["callback"] = "showDialog2"});
+
+  sourcePath = debug.getinfo(1).source:match("@?(.*/)")
+  print(sourcePath)
 end
 
 
 local currentFill = false
 local currentRotsnap = false
 local currentGridsnap = false 
+
+
+
+function showDialog2()
+  print("0 llega hasta aca")
+  local hasLgi, lgi = pcall(require, "lgi")
+  if not hasLgi then
+    app.msgbox("You need to have the Lua lgi-module installed and included in your Lua package path in order to use the GUI for migrating font sizes. \n\n", {[1]="OK"})
+    return
+  end
+  print("1")
+  --lgi module has been found
+  local Gtk2 = lgi.Gtk
+  print("1.1")
+  local Gdk = lgi.Gdk
+  print("1.2")
+  local assert = lgi.assert
+  print("2")
+  local builder = Gtk.Builder()
+  assert(builder:add_from_file(sourcePath .. "dialog.glade"))
+  local ui = builder.objects
+  print("3")
+  local dialog = ui.dlgKeys
+
+  print("99 llega hasta aca")
+
+  -- if not currDpi then 
+  --   currDpi = app.getDisplayDpi()
+  -- end
+  -- ui.spbtOldDpi:set_value(currDpi)
+  -- ui.lblCurrentDpi:set_text(app.getDisplayDpi())
+  -- ui.spbtScaleFactor:set_value(currDpi/72)
+
+-- Connect actions
+  function ui.btSave.on_clicked()
+    local a = 0
+    -- local factor = ui.spbtScaleFactor:get_value()
+    -- resize(factor)
+  end
+
+  function ui.btCancel.on_clicked()
+    dialog:destroy()
+  end
+
+  -- function ui.spbtScaleFactor.on_value_changed()
+  --   factor = ui.spbtScaleFactor:get_value()
+  --   currDpi = math.floor(factor*72+0.5)
+  --   ui.spbtOldDpi:set_value(currDpi)
+ --  end
+
+  -- function ui.spbtOldDpi.on_value_changed()
+  --   oldDpi = ui.spbtOldDpi:get_value()
+  --   ui.spbtScaleFactor:set_value(oldDpi/72)
+  -- end
+
+  dialog:show_all()
+end 
 
 function gridsnap()
   currentGridsnap = not currentGridsnap
@@ -75,29 +143,60 @@ local currentDrawingtype = 1
 --   print("Color: " .. colorList[currentColor][1])
 -- end
 
-function colorN(currentColor)
-  app.changeToolColor({["color"] = colorList[currentColor][2], ["selection"] = true})
-  print("Color: " .. colorList[currentColor][1])
+-- function colorN(currentColor)
+--   app.changeToolColor({["color"] = colorList[currentColor][2], ["selection"] = true})
+--   print("Color: " .. colorList[currentColor][1])
+-- end
+
+function colorN(hexColor)
+  app.changeToolColor({["color"] = hexColor, ["selection"] = true})
+  print("Color: " .. hexColor)
 end
 
 function color1()
-  currentColor = 9 -- "orange"
-  colorN(currentColor)
+  hexColor = 0xFFFFFF -- "white"
+  colorN(hexColor)
 end
 
 function color2()
-  currentColor = 3 -- "lightblue"
-  colorN(currentColor)
+  hexColor = 0xFF8000 -- "orange"
+  colorN(hexColor)
 end
 
 function color3()
-  currentColor = 10 -- "yellow"
-  colorN(currentColor)
+  hexColor = 0x00C0FF -- "light blue"
+  colorN(hexColor)
 end
 
 function color4()
-  currentColor = 8 -- "magenta"
-  colorN(currentColor)
+  hexColor = 0xFFEC30 -- "yellow"
+  colorN(hexColor)
+end
+
+function color5()
+  hexColor = 0xF61F1F -- "red"
+  colorN(hexColor)
+end
+function color6()
+  hexColor = 0x73D216 -- "green"
+  colorN(hexColor)
+end
+function color7()
+  hexColor = 0xA44D9C -- "violet"
+  colorN(hexColor)
+end
+function color8()
+  hexColor = 0xFF43ED -- "pink"
+  colorN(hexColor)
+end
+function color9()
+  hexColor = 0x6C67FF -- "blue"
+  colorN(hexColor)
+end
+
+function color0()
+  hexColor = 0x656565 -- "gray"
+  colorN(hexColor)
 end
 
 function linestyle()
